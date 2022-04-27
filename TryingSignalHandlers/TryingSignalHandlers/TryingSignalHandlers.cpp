cb8519fda0c6e6f2eab7c5a019503144dcee7d8d
Yudjerick <denispetrenko2004@gmail.com>
2022-04-27 23:30:14 +0300

    for lab OOP 33

---
+++ TryingSignalHandlers/TryingSignalHandlers/TryingSignalHandlers.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#define TYPE_SIGNAL(a) void (B::*a) ()
+#define FOR(n) for(int i = 0; i < n; i++)
+using namespace std;
+
+class B
+{
+public:
+    void handler() {
+        cout << "handler called";
+    }
+};
+
+class A 
+{
+public:
+    B* b;
+    void signal(TYPE_SIGNAL(a)) {
+        cout << "signal called";
+        (b->*a)();
+    }
+};
+
+int main()
+{
+    A* obj1 = new A;
+    B* obj2 = new B;
+    obj1->b = obj2;
+    obj1->signal(&B::handler);
+}
